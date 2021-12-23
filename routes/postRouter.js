73ff4d76293759a98fb2318b9444ff639d73e64e
tmhuyett <toddhuyett@gmail.com>
2021-12-23 00:24:45 -0500

    'd'

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -60,15 +60,16 @@ postRouter
           err => next(err)
         )
         .catch(err => {
-        console.log("shit" + err.message)  
-          next(err)})
+          console.log("shit" + err.message)
+          next(err)
+        })
     }
   )
 
 postRouter
   .route("/:slug")
   .get((req, res, next) => {
-    Posts.find({slug: req.params.slug})
+    Posts.find({ slug: req.params.slug })
       .populate("comments.author")
       .then(
         post => {
@@ -119,7 +120,7 @@ postRouter
     }
   )
 
-  postRouter
+postRouter
   .route("/:postId")
   .get((req, res, next) => {
     Posts.findById(req.params.postId)
@@ -133,11 +134,11 @@ postRouter
       )
       .catch(err => next(err))
   })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403
     res.end("POST operation not supported on /leaders/" + req.params.leaderId)
   })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Posts.findByIdAndUpdate(
       req.params.postId,
       {
@@ -155,216 +156,16 @@ postRouter
       )
       .catch(err => next(err))
   })
-  .delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
-    Posts.findByIdAndRemove(req.params.postId)
-      .then(
-        resp => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(resp)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-
-postRouter
-  .route("/:postId/comments")
-  .get((req, res, next) => {
-    Posts.findById(req.params.postId)
-      .populate("comments.author")
-      .then(
-        post => {
-          if (post != null) {
-            res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
-            res.json(post.comments)
-          } else {
-            err = new Error("post " + req.params.postId + " not found")
-            err.status = 404
-            return next(err)
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Posts.findById(req.params.postId)
-      .then(
-        post => {
-          if (post != null) {
-            req.body.author = req.user._id
-            post.comments.push(req.body)
-            post.save().then(
-              post => {
-                Posts.findById(post._id)
-                  .populate("comments.author")
-                  .then(post => {
-                    res.statusCode = 200
-                    res.setHeader("Content-Type", "application/json")
-                    res.json(post)
-                  })
-              },
-              err => next(err)
-            )
-          } else {
-            err = new Error("post " + req.params.postId + " not found")
-            err.status = 404
-            return next(err)
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403
-    res.end(
-      "PUT operation not supported on /posts/" + req.params.postId + "/comments"
-    )
-  })
   .delete(
     authenticate.verifyUser,
     authenticate.verifyAdmin,
     (req, res, next) => {
-      Posts.findById(req.params.postId)
+      Posts.findByIdAndRemove(req.params.postId)
         .then(
-          post => {
-            if (post != null) {
-              for (var i = post.comments.length - 1; i >= 0; i--) {
-                post.comments.id(post.comments[i]._id).remove()
-              }
-              post.save().then(
-                post => {
-                  res.statusCode = 200
-                  res.setHeader("Content-Type", "application/json")
-                  res.json(post)
-                },
-                err => next(err)
-              )
-            } else {
-              err = new Error("post " + req.params.postId + " not found")
-              err.status = 404
-              return next(err)
-            }
-          },
-          err => next(err)
-        )
-        .catch(err => next(err))
-    }
-  )
-
-postRouter
-  .route("/:postId/comments/:commentId")
-  .get((req, res, next) => {
-    Posts.findById(req.params.postId)
-      .populate("comments.author")
-      .then(
-        post => {
-          if (post != null && post.comments.id(req.params.commentId) != null) {
+          resp => {
             res.statusCode = 200
             res.setHeader("Content-Type", "application/json")
-            res.json(post.comments.id(req.params.commentId))
-          } else if (post == null) {
-            err = new Error("post " + req.params.postId + " not found")
-            err.status = 404
-            return next(err)
-          } else {
-            err = new Error("Comment " + req.params.commentId + " not found")
-            err.status = 404
-            return next(err)
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403
-    res.end(
-      "POST operation not supported on /posts/" +
-        req.params.postId +
-        "/comments/" +
-        req.params.commentId
-    )
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Posts.findById(req.params.postId)
-      .then(
-        post => {
-          if (
-            post != null &&
-            post.comments.id(req.params.commentId) != null &&
-            post.comments.id(req.params.commentId).author == req.user._id
-          ) {
-            if (req.body.rating) {
-              post.comments.id(req.params.commentId).rating = req.body.rating
-            }
-            if (req.body.comment) {
-              post.comments.id(req.params.commentId).comment = req.body.comment
-            }
-            post.save().then(
-              post => {
-                Posts.findById(post._id)
-                  .populate("comments.author")
-                  .then(post => {
-                    res.statusCode = 200
-                    res.setHeader("Content-Type", "application/json")
-                    res.json(post)
-                  })
-              },
-              err => next(err)
-            )
-          } else if (post == null) {
-            err = new Error("post " + req.params.postId + " not found")
-            err.status = 404
-            return next(err)
-          } else {
-            err = new Error("Comment " + req.params.commentId + " not found")
-            err.status = 404
-            return next(err)
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .delete(
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Posts.findById(req.params.postId)
-        .then(
-          post => {
-            if (
-              post != null &&
-              post.comments.id(req.params.commentId) != null &&
-              post.comments.id(req.params.commentId).author == req.user._id
-            ) {
-              post.comments.id(req.params.commentId).remove()
-              post.save().then(
-                post => {
-                  Posts.findById(post._id)
-                    .populate("comments.author")
-                    .then(post => {
-                      res.statusCode = 200
-                      res.setHeader("Content-Type", "application/json")
-                      res.json(post)
-                    })
-                },
-                err => next(err)
-              )
-            } else if (post == null) {
-              err = new Error("post " + req.params.postId + " not found")
-              err.status = 404
-              return next(err)
-            } else {
-              err = new Error("Comment " + req.params.commentId + " not found")
-              err.status = 404
-              return next(err)
-            }
+            res.json(resp)
           },
           err => next(err)
         )
