2f8d8ef8c3d5fe0cc4683dbfabf9937e1ab81822
tmhuyett <toddhuyett@gmail.com>
2022-02-01 21:30:03 -0500

    'd'

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -6,61 +6,42 @@ const jobRouter = express.Router()
 jobRouter.use(bodyParser.json())
 
 jobRouter
-  .route("/")
-  .get(authenticate.verifyUser, (req, res, next) => {
-    Jobs.findOne({ user: req.user._id })
-      .populate("user")
-      .populate("jobs")
-      .then(
-        jobs => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(jobs)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .post(authenticate.verifyUser, (req, res, next) => {
-    Jobs.findOne({ user: req.user._id })
-      .then(
-        item => {
-          if (item) {
-            for (var i = 0; i < req.body.length; i++) {
-              let n = req.body[i]._id
-              if (item.jobs.indexOf(n) === -1) {
-                item.jobs.push(n)
-              }
-            }
-            item.save().then(
-              item => {
-                res.statusCode = 200
-                res.setHeader("Content-Type", "application/json")
-                res.json(item)
-              },
-              err => next(err)
-            )
-          } else {
-            jobs.create({ user: req.user._id, jobs: req.body }).then(
-              item => {
-                res.statusCode = 200
-                res.setHeader("Content-Type", "application/json")
-                res.json(favorite)
-              },
-              err => next(err)
-            )
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
+.route("/")
+.get((req, res, next) => {
+  Jobs.find({})
+    .then(
+      jobs => {
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        res.json(jobs)
+      },
+      err => next(err)
+    )
+    .catch(err => next(err))
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+  Jobs.create(req.body)
+    .then(
+      job => {
+        console.log("post Created ", job)
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        res.json(job)
+      },
+      err => {
+        res.status(500).send(err)
+      }
+    )
+    .catch(err => {
+      next(err)
+    })
+})
   .put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403
     res.end("PUT operation not supported on /jobs")
   })
   .delete(authenticate.verifyUser, (req, res, next) => {
-    jobs.findOneAndRemove({ user: req.user._id })
+    Jobs.findOneAndRemove({ user: req.user._id })
       .then(
         responze => {
           res.statusCode = 200
@@ -98,7 +79,7 @@ jobRouter
               )
             }
           } else {
-            jobs.create({
+            Jobs.create({
               user: req.user._id,
               jobs: [req.params.jobId],
             }).then(
