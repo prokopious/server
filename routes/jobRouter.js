8742f1f45ac004825fb89c1025f3412c104d8333
tmhuyett <toddhuyett@gmail.com>
2022-02-03 14:46:38 -0500

    'd'

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -43,51 +43,29 @@ jobRouter
 
 jobRouter
 .route("/:jobId")
-.get((req, res, next) => {
-  Jobs.findById(req.params.jobId)
-    .then(
-      job => {
-        res.statusCode = 200
-        res.setHeader("Content-Type", "application/json")
-        res.json(job)
-      },
-      err => next(err)
-    )
-    .catch(err => next(err))
+.post(authenticate.verifyUser, (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-  Jobs.findOneAndUpdate(
-    req.params.jobId,
-    {
-      $set: req.body,
-    },
-    { new: true }
-  )
-    .then(
-      job => {
-        res.statusCode = 200
-        res.setHeader("Content-Type", "application/json")
-        res.json(job)
-      },
-      err => next(err)
-    )
-    .catch(err => next(err))
+.put(authenticate.verifyUser, (req, res, next) => {
+  Jobs.findByIdAndUpdate(req.params.jobId, {
+      $set: req.body
+  }, { new: true })
+  .then((job) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(job);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
-.delete(
-  authenticate.verifyUser,
-  authenticate.verifyAdmin,
-  (req, res, next) => {
-    Jobs.findByIdAndRemove(req.params.jobId)
-      .then(
-        resp => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(resp)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  }
-)
+.delete(authenticate.verifyUser, (req, res, next) => {
+  Jobs.findByIdAndRemove(req.params.jobId)
+  .then((resp) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
 
 module.exports = jobRouter
