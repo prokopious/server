d57ae8e1b06b29a477655634eee6c48f06c5d649
tmhuyett <toddhuyett@gmail.com>
2022-02-03 11:49:41 -0500

    'd'

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -54,87 +54,59 @@ jobRouter
   })
 
 jobRouter
-  .route("/:jobId")
-  .options((req, res) => {
-    res.sendStatus(200)
-  })
-  .get((req, res, next) => {
-    Jobs.findById(req.params.jobId)
-    .then((job) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(job);
-    }, (err) => next(err))
-    .catch((err) => next(err));
+.route("/:jobId")
+.get((req, res, next) => {
+  Jobs.findById(req.params.jobId)
+    .then(
+      recruiter => {
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        res.json(job)
+      },
+      err => next(err)
+    )
+    .catch(err => next(err))
 })
-  .post(authenticate.verifyUser, (req, res, next) => {
-    Jobs.findOne({ user: req.user._id })
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  res.statusCode = 403
+  res.end(
+    "recruiter operation not supported on /recruiters/" +
+      req.params.jobId
+  )
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  Jobs.findOneAndUpdate(
+    req.params.recruiterId,
+    {
+      $set: req.body,
+    },
+    { new: true }
+  )
+    .then(
+      job => {
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        res.json(job)
+      },
+      err => next(err)
+    )
+    .catch(err => next(err))
+})
+.delete(
+  authenticate.verifyUser,
+  authenticate.verifyAdmin,
+  (req, res, next) => {
+    Jobs.findByIdAndRemove(req.params.jobId)
       .then(
-        item => {
-          if (item) {
-            if (item.jobs.indexOf(req.params.jobId) === -1) {
-              item.jobs.push(req.params.jobId)
-              item.save().then(
-                item => {
-                  res.statusCode = 200
-                  res.setHeader("Content-Type", "application/json")
-                  res.json(item)
-                },
-                err => next(err)
-              )
-            }
-          } else {
-            Jobs.create({
-              user: req.user._id,
-              jobs: [req.params.jobId],
-            }).then(
-              item => {
-                res.statusCode = 200
-                res.setHeader("Content-Type", "application/json")
-                res.json(favorite)
-              },
-              err => next(err)
-            )
-          }
+        resp => {
+          res.statusCode = 200
+          res.setHeader("Content-Type", "application/json")
+          res.json(resp)
         },
         err => next(err)
       )
       .catch(err => next(err))
-  })
-  .put(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403
-    res.end(req.params.jobId)
-  })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    Jobs.findOne({ user: req.user._id })
-      .then(
-        item => {
-          if (item) {
-            index = item.jobs.indexOf(req.params.jobId)
-            if (index >= 0) {
-              item.jobs.splice(index, 1)
-              item.save().then(
-                item => {
-                  res.statusCode = 200
-                  res.setHeader("Content-Type", "application/json")
-                  res.json(item)
-                },
-                err => next(err)
-              )
-            } else {
-              err = new Error("error")
-              err.status = 404
-              return next(err)
-            }
-          } else {
-            err = new Error("error")
-            err.status = 404
-            return next(err)
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
+  }
+)
 
 module.exports = jobRouter
--- routes/recruiterRouter.js
+++ routes/recruiterRouter.js
@@ -118,61 +118,6 @@ recruiterRouter
         .catch(err => next(err))
     }
   )
-recruiterRouter
-  .route("/:recruiterId")
-  .get((req, res, next) => {
-    Recruiters.find({ slug: req.params.recruiterId })
-      .populate("comments.author")
-      .then(
-        recruiter => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(recruiter)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403
-    res.end(
-      "recruiter operation not supported on /recruiters/" +
-        req.params.recruiterId
-    )
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Recruiters.findOneAndUpdate(
-      req.params.recruiterId,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    )
-      .then(
-        recruiter => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(recruiter)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .delete(
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Recruiters.findByIdAndRemove(req.params.recruiterId)
-        .then(
-          resp => {
-            res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
-            res.json(resp)
-          },
-          err => next(err)
-        )
-        .catch(err => next(err))
-    }
-  )
+
 
 module.exports = recruiterRouter
