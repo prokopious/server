a15a6a2c559ba8b682f9a026c7c8207d4eeab1b5
tmhuyett <toddhuyett@gmail.com>
2022-02-03 16:25:06 -0500

    'd'

--- routes/recruiterRouter.js
+++ routes/recruiterRouter.js
@@ -44,52 +44,39 @@ recruiterRouter
     res.end("PUT operation not supported on /recruiters")
   })
 recruiterRouter
-  .route("/:recruiterId")
-  .get((req, res, next) => {
-    Recruiters.findById(req.params.recruiterId)
-      .then(
-        recruiter => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(recruiter)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Recruiters.findOneAndUpdate(
-      req.params.recruiterId,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    )
-      .then(
-        recruiter => {
-          res.statusCode = 200
-          res.setHeader("Content-Type", "application/json")
-          res.json(recruiter)
-        },
-        err => next(err)
-      )
-      .catch(err => next(err))
-  })
-  .delete(
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Recruiters.findByIdAndRemove(req.params.recruiterId)
-        .then(
-          resp => {
-            res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
-            res.json(resp)
-          },
-          err => next(err)
-        )
-        .catch(err => next(err))
-    }
-  )
+.route("/:recruiterId")
+.get((req, res, next) => {
+  Recruiters.findById(req.params.recruiterId)
+      .then((recruiter) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(recruiter);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+  Recruiters.findByIdAndUpdate(req.params.recruiterId, {
+      $set: req.body
+  }, { new: true })
+  .then((recruiter) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(recruiter);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+.delete(authenticate.verifyUser, (req, res, next) => {
+  Recruiters.findByIdAndRemove(req.params.recruiterId)
+  .then((resp) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
 
 module.exports = recruiterRouter
